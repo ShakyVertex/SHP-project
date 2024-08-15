d9f5d9551b20e503e936b5c3b59336e873d1be49
Kangning <sellamomota@foxmail.com>
2024-08-15 16:04:24 -0400

    dsafa

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -1,5 +1,7 @@
 // 对所有的api接口进行统一管理
 import requests from './request'
+import mockRequests from './mockAjax'
 
 // 三级联动接口
-export const reqCategoryList = () => requests({url: '/product/getBaseCategoryList', method: 'get'})
+export const reqCategoryList = () => requests.get('/product/getBaseCategoryList')
+export const reqGetBannerList = () => mockRequests.get('/banner')
